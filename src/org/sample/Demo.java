1f5add481e4c7f7c4af7d3deb8d2c2d4a2d4a946
vinoth <vinothartha@gmail.com>
2021-11-28 20:47:35 +0530

    sample

---
+++ src/org/sample/Demo.java
@@ -0,0 +1,17 @@
+package org.sample;
+
+public class Demo {
+
+	public static void main(String[] args) {
+
+		String s = "Welcome";
+		String rev = "";
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			char charAt = s.charAt(i);
+			rev = rev + charAt;
+		}
+		System.out.println(rev);
+	}
+
+}
